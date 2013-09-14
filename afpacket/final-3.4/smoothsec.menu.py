a3b6420d3822576a015aaddc61d6ba71c7248651
samiux <runnersam@gmail.com>
2013-09-14 16:24:21 +0800

    Amended.

--- afpacket/final-3.4/smoothsec.menu.py
+++ afpacket/final-3.4/smoothsec.menu.py
@@ -169,7 +169,7 @@ def script_update():
 
 	print " [>] Updating this script.\n"
 	
-	subprocess.Popen("wget https://github.com/smoothsec/dev/blob/samiux/afpacket/final-3.4/smoothsec.menu.py",shell=True).wait()
+	subprocess.Popen("wget https://raw.github.com/smoothsec/dev/samiux/afpacket/final-3.4/smoothsec.menu.py",shell=True).wait()
 	subprocess.Popen("chmod a+x smoothsec.menu.py",shell=True).wait()
 	print "\n"
 	print " [>] Update successfully, now please run the script again!\n"
