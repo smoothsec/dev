8d1652b03ab471c9a1087c086efec6d48c9a48ce
samiux <runnersam@gmail.com>
2013-09-14 17:11:58 +0800

    Amended.

--- afpacket/final-3.4/smoothsec.menu.py
+++ afpacket/final-3.4/smoothsec.menu.py
@@ -180,11 +180,11 @@ def script_update():
 	subprocess.Popen("wget https://raw.github.com/smoothsec/dev/samiux/afpacket/final-3.4/smoothsec.menu.py -O smoothsec.menu.py.new",shell=True).wait()
 	subprocess.Popen("mv smoothsec.menu.py.new smoothsec.menu.py",shell=True).wait()
 	subprocess.Popen("chmod a+x smoothsec.menu.py",shell=True).wait()
-	subprocess.Popen("mv smoothsec.menu.py /usr/local/sbin/smoothsec.menu.py",shell=True).wait()
+	subprocess.Popen("mv smoothsec.menu.py /usr/local/sbin/smoothsec.menu",shell=True).wait()
 	print "\n"
 	print " [>] Update successfully, now please run the script again!\n"
 	sleep(2)
-	subprocess.Popen("smoothsec.menu.py",shell=True).wait()
+	subprocess.Popen("smoothsec.menu",shell=True).wait()
 	
 def tryharder():
 
